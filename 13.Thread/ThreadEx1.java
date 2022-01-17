a52187e506d5d1e7b56017c18ce6d279213e78d2
daeuun <daeuun>
2022-01-18 01:23:26 +0900

    thread

---
+++ 13.Thread/ThreadEx1.java
@@ -0,0 +1,11 @@
+class ThreadEx1 {
+    public static void main(String[] args) {
+        ThreadEx1_1 t1 = new ThreadEx1_1();
+        
+        Runnable r = new Thread1_2();
+        Thread t2 = new Thread(r); // Runnable target
+        
+        t1.start(); // thread 실행 : 하나의 쓰레드 - ㄴ
+        t2.start();
+    }
+}
