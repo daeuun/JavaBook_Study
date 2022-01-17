7210a96a75cea8d43c49636ab8a69e48d9d2d5f2
daeuun <daeuun>
2022-01-18 01:36:12 +0900

    git pull origin master

--- 13.Thread/ThreadEx1.java
+++ 13.Thread/ThreadEx1.java
@@ -4,8 +4,8 @@ class ThreadEx1 {
         
         Runnable r = new Thread1_2();
         Thread t2 = new Thread(r); // Runnable target
-        
+
         t1.start(); // thread 실행 : 하나의 쓰레드 - ㄴ
-        t2.start();
+        t2.start(); 
     }
 }
